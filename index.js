078e351ec8f5d95989ab994b088b27439ca27b42
AriamGoitom <117870540+AriamGoitom@users.noreply.github.com>
2026-05-03 23:16:59 +0200

    add a function for login

---
+++ index.js
@@ -0,0 +1,26 @@
+const identifier = document.querySelector("#identifier");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("#loginBtn");
+const registerUsername = document.querySelector("#registerUsername");
+const registerEmail = document.querySelector("#registerEmail");
+const registerPassword = document.querySelector("#registerPassword");
+const registerBtn = document.querySelector("#registerBtn");
+const logoutBtn = document.querySelector("#logoutBtn");
+const userInfo = document.querySelector("#user-info");
+const booksContainer = document.querySelector("#books-container");
+
+const login = async () => {
+    try {
+        const response = await axios.post("http://localhost:1337/api/auth/local", {
+            identifier: identifier.value,
+            password: password.value
+        });
+        localStorage.setItem("token", response.data.jwt);
+        localStorage.setItem("user", JSON.stringify(response.data.user));
+
+        checkLogin();
+    }
+    catch(error){
+        alert("Wrong login");
+    }
+};
